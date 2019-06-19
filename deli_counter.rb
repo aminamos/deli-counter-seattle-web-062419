f08eccbe8aec6c190d129246305ba82b0bb08e96
Amin Amos <aminamosahs@gmail.com>
2019-06-19 17:01:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
-    "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     until katz_deli.length == 0
       puts "Currently serving #{katz_deli.first}."
