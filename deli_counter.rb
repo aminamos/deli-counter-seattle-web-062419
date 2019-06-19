99c4dadf7a1cb54209686fc9ce34f4fe587331d6
Amin Amos <aminamosahs@gmail.com>
2019-06-19 17:32:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  newarray = ["The line is currently: "]
+  newarray = ["The line is currently:"]
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
