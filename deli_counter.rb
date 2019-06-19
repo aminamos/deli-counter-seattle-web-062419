6f970ed2203c014f8a4199877a3e1448673f2425
Amin Amos <aminamosahs@gmail.com>
2019-06-19 17:15:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
       until counter == katz_deli.length
-        katz_deli.each {|i| puts "The line is currently: #{katz_deli.index(i)}. #{katz_deli[i]}."}
+        katz_deli.each.with_index {|i| puts "The line is currently: #{katz_deli.index(i)}. #{katz_deli[i]}."}
       end
   end
 end
